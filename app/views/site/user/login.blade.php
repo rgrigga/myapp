8e493e17e9a26f5b7a237e7abbfdd66d9107496f
Ryan Grissinger <ryan.grissinger@gmail.com>
2013-06-30 04:53:59 -0400

    fixed login permissions

--- app/views/site/user/login.blade.php
+++ app/views/site/user/login.blade.php
@@ -18,6 +18,8 @@
 <div class="page-header">
 	<h1>Login into your account here</h1>
 </div>
+
+<p>By the way, we only ask for email to verify that you are you.  You can also login with Google.</p>
 <form method="POST" action="{{ URL::to('user/login') }}" accept-charset="UTF-8">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <fieldset>
