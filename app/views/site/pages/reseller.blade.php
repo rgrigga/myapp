fc8ea060ca4b0db1f7a587c6e80ab0868bdae23d
Ryan Grissinger <ryan.grissinger@gmail.com>
2013-07-01 02:08:16 -0400

    begin reseller

---
+++ app/views/site/pages/reseller.blade.php
@@ -0,0 +1,17 @@
+@extends('site.layouts.default')
+{{-- Web site Title --}}
+@section('title')
+@parent
+{{{ "Reseller" }}} :: Google Apps
+@parent
+@stop
+
+{{-- Content --}}
+@section('content')
+
+
+<gcse:search></gcse:search>
+
+The 
+
+@stop
