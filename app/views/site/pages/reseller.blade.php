5cfb0f993c6fad8cc7e2e596e867355ea138a206
Ryan Grissinger <ryan.grissinger@gmail.com>
2013-07-01 02:27:20 -0400

    reseller n routes

--- app/views/site/pages/reseller.blade.php
+++ app/views/site/pages/reseller.blade.php
@@ -2,14 +2,17 @@
 {{-- Web site Title --}}
 @section('title')
 @parent
-{{{ "Reseller" }}} :: Google Apps
-@parent
+ :: {{{ "Reseller" }}} :: Google Apps
 @stop
 
+@section('google')
+@parent
+<meta name="google-site-verification" content="MDmmPWcnfgrGGWK9qZavWe3BYBPkzeScRj9KttTy03Y" />
+@stop
 {{-- Content --}}
 @section('content')
 
-foobar
+Welcome to the reseller page.
 
 <gcse:search></gcse:search>
 
