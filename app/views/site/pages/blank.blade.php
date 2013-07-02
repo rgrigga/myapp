c20340c03a60efe40e7ad1aef47b1d413352e4ec
Ryan Grissinger <ryan.grissinger@gmail.com>
2013-07-02 18:15:33 -0400

    about

--- app/views/site/pages/blank.blade.php
+++ app/views/site/pages/blank.blade.php
@@ -1,26 +1,13 @@
 @extends('site.layouts.default')
 {{-- Web site Title --}}
-@section('title')
-{{{ "Maintenance" }}} ::
+@section('meta-title')
+{{{ "Blankity-Blank" }}} ::
 @parent
 @stop
 
 {{-- Content --}}
 @section('content')
 
-<script>
-//Google Custom Search
-  // (function() {
-  //   var cx = '012277204628171564007:sik_hha9myk';
-  //   var gcse = document.createElement('script');
-  //   gcse.type = 'text/javascript';
-  //   gcse.async = true;
-  //   gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-  //       '//www.google.com/cse/cse.js?cx=' + cx;
-  //   var s = document.getElementsByTagName('script')[0];
-  //   s.parentNode.insertBefore(gcse, s);
-  // })();
-</script>
-<!-- <gcse:search></gcse:search> -->
+Welcome to a blank page.
 
 @stop
