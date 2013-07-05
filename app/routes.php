fc9455374e46b3c393b33a87a8edc329259bb86b
Ryan Grissinger <ryan.grissinger@gmail.com>
2013-07-05 00:47:06 -0400

    this is a test

--- app/routes.php
+++ app/routes.php
@@ -10,7 +10,7 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
+// bam!
 /** ------------------------------------------
  *  Route model binding
  *  ------------------------------------------
