f4f3b3928a150a8b235d56c66d74b1a2f02f0188
Ryan Grissinger <ryan.grissinger@gmail.com>
2013-07-10 16:38:49 -0400

    layout work

---
+++ app/views/site/partials/contact-buckeye.blade.php
@@ -0,0 +1,29 @@
+<div class="iconbar pull-right" style="padding-top:5px">
+<!-- 	<div class="iconbutton">
+		
+	</div> -->
+    <a href="mailto:russ@buckeyemower.com" class="social-icon">
+        <i class="icon-envelope-alt icon-2x"></i>
+        <!-- <img src="http://gristech.com/buttons/email.png" class="img-circle"> -->
+    </a>
+    <a href="tel:7405076198" class="social-icon">
+        <i class="icon-phone-sign icon-2x"></i>
+        <!-- <img src="http://gristech.com/buttons/email.png" class="img-circle"> -->
+    </a>
+	<a href="http://facebook.com/BuckeyeMower" class="social-icon">
+	    <!-- <img src="http://gristech.com/img/facebook.png" class="img-circle"> -->
+		<i class="icon-facebook-sign icon-2x"></i>
+	</a>
+
+<!-- TWITTER -->
+    <!-- <a href="http://twitter.com/ryangrissinger" class="social-icon"> -->
+    	<!-- <i class="icon-twitter-sign icon-2x"></i> -->
+        <!-- <img src="http://gristech.com/img/twitter.png" class="img-circle"> -->
+    <!-- </a> -->
+
+<!-- LINKEDIN -->
+    <!-- <a href="http://linkedin.com/ryangrissinger" class="social-icon"> -->
+    	<!-- <i class="icon-linkedin-sign icon-2x"></i> -->
+        <!-- <img src="http://gristech.com/buttons/linkedin.png" class="img-circle"> -->
+    <!-- </a> -->
+</div>
