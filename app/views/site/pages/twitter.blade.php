34776f1ec8ef601a2144388083935430879723fa
Ryan Grissinger <ryan.grissinger@gmail.com>
2013-07-20 09:33:02 -0400

    misse da few

---
+++ app/views/site/pages/twitter.blade.php
@@ -0,0 +1,21 @@
+<!-- twitter.blade.php -->
+<h1>Twitter</h1>
+<a href="https://dev.twitter.com/terms/display-requirements">Developer Display Requirements</a>
+<a href="https://twitter.com/logo#twitter-content">Content Display Rules</a>
+
+<a href="https://dev.twitter.com/docs/embedded-tweets">Embedded Tweets</a>
+
+<h2>Examples</h2>
+<div class="row">
+	<div class="span4">
+		<a class="twitter-timeline"  href="https://twitter.com/ryangrissinger/favorites"  data-widget-id="330243075756658691">Favorite Tweets by @ryangrissinger</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+	</div>
+	<div class="span4">
+		
+	</div>
+	<div class="span4">
+		
+	</div>
+</div>
